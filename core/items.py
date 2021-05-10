d6b85154a5e9209b3940cb8a09756b357ed9ba2c
Mehrshad <m-b850@protonmail.com>
2021-05-10 06:30:34 +0430

    Comments Fields added and updated.

--- core/items.py
+++ core/items.py
@@ -32,7 +32,20 @@ def parse(data):
     comments = []
     for d in data:
         raw_comments = Soup(d.text, 'html.parser', parse_only=only_item_cells)
-        print(raw_comments)
+    for r in raw_comments:
+        """ Each comment-box in page """
+        ids = r.find_all('div', {'class': 'info'})
+        descriptions = r.find_all('p', {'class': 'CommentsStyle1'})
+        # There are n comments/descriptions in each comment-box
+        n = len(descriptions)
+        for i in range(n):
+            """ Each comment box content """
+            # It contains datetime so spilt them by \n
+            CommentOwnerId = ids[i].text.strip().split('\n')[0]
+            CommentDate = ids[i].text.strip().split('\n')[1]
+            CommentDescription = descriptions[i].text.strip()
+            # print(CommentDate)
+            # print('---------------')
     return comments
 
 
